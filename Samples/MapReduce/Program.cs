f2ec0504126078f550521361684dfc37318129fc
Owen Evans <owenevans00@gmail.com>
2016-10-02 06:45:45 -0400

    Added a MapReduce sample

---
+++ Samples/MapReduce/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actor;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace MapReduce
+{
+    class Program
+    {
+
+        /*
+         * Map/Reduce with actors: this example calculates the frequency of words in
+         * the "Friends, Romans, Countrymen" speech from Julius Caesar.
+         * 
+         * the corpus is mapped across 4 actors, each of which produce a 
+         * dictionary of <word,frequency>, and those results are then reduced down to
+         * a single combined dictionary. That dictionary is then formwarded to another
+         * for output.
+         */
+        static void Main(string[] args)
+        {
+            // this is the actual mapping operation, which is fed into a SimpleActor
+            // instead of building an entire subclass. Note that this is only possible
+            // to do when the actor is stateless 
+            var mapFunc = new Action<Message<string>>(msg =>
+            {
+                var processed = msg.State
+                    .Split(' ')
+                    .Select(s => Regex.Replace(s, "\\W", "").ToLower())
+                    .GroupBy(s => s)
+                    .ToDictionary(
+                        g => g.Key,
+                        g => g.Count()
+                    );
+                msg.ReplyTo.Post(processed);
+            });
+
+            var mapperList = new IActor[] {
+                new SimpleActor<string>(mapFunc),
+                new SimpleActor<string>(mapFunc),
+                new SimpleActor<string>(mapFunc),
+                new SimpleActor<string>(mapFunc)
+            };
+
+            var reduce = new ReduceActor();
+
+            using (var corpus = new StreamReader(@".\corpus.txt"))
+            {
+                int idx = 0;
+
+                while (!corpus.EndOfStream)
+                {
+                    var mapperId = idx % mapperList.Length;
+                    mapperList[mapperId].Post(
+                        new Message<string>(corpus.ReadLine(), reduce)
+                    );
+                    idx++;
+                }
+
+                reduce.ExpectedMessages = idx;
+                reduce.Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+
+}
---
+++ Samples/MapReduce/ReduceActor.cs
@@ -0,0 +1,65 @@
+﻿using Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MapReduce
+{
+    /*
+    * Takes messages containing word frequency dictionaries and merges
+    * them into one. This implementation expects to have all the data
+    * queued up so it can tell if it's done; finding more sophisticated 
+    * ways to manage that is left as an exercise for the user
+    */
+    public class ReduceActor : ActorBase
+    {
+        int count = 0;
+        Dictionary<string, int> wordCount;
+
+        public int ExpectedMessages { get; internal set; }
+
+        public ReduceActor() : base(false)
+        {
+            wordCount = new Dictionary<string, int>();
+            AddState<Dictionary<string, int>>(Merge);
+        }
+
+        private void Merge(Message<Dictionary<string, int>> msg)
+        {
+            var dict = msg.State;
+            foreach (var k in dict.Keys)
+            {
+                int val;
+                if (wordCount.TryGetValue(k, out val))
+                    wordCount[k] = val + dict[k];
+                else
+                    wordCount.Add(k, dict[k]);
+            }
+            count++;
+
+            // if done, output the 20 most common words
+            if (count == ExpectedMessages)
+                new OutputActor(20).Post(wordCount);
+        }
+    }
+
+    internal class OutputActor : ActorBase
+    {
+        int _topN;
+
+        protected internal OutputActor(int topN) : base(true)
+        {
+            _topN = topN;
+            AddState<Dictionary<string, int>>(Print);
+        }
+
+        private void Print(Message<Dictionary<string, int>> msg)
+        {
+            Console.WriteLine($"{_topN} most common words in corpus");
+            foreach (var kv in msg.State.Select(kv => kv).OrderByDescending(kv => kv.Value).Take(_topN))
+            {
+                Console.WriteLine("{0}\t{1}", kv.Key, kv.Value);
+            }
+        }
+    }
+}
